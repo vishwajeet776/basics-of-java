b6cc95f95c4f057fd86baa0a81e9b85a8baf9da5
vishw <vishwajeet27@gmail.com>
2023-06-11 10:40:45 +0530

    initial version

---
+++ src/selection_sort.java
@@ -0,0 +1,26 @@
+public class selection_sort {
+    public static void main(String[] args) {
+        int arr[]={7,8,1,3,2};
+//        for (int i=0;i<arr.length;i++){
+//            System.out.print(arr[i]+" ");
+//        }
+//        System.out.println();
+        for (int i=0;i<arr.length-1;i++){
+            int small=i;
+            for (int j=i+1;j<arr.length;j++){
+                if (arr[small]>arr[j]){
+                    small=j;
+                }
+            }
+            int temp=arr[small];
+            arr[small]=arr[i];
+            arr[i]=temp;
+
+        }
+        for (int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+
+    }
+}
